34e3e72f2cd979ea461a1508d534e361b9e3b612
Libx09 <ibrahimfuad729@gmail.com>
2024-09-24 01:17:07 +0600

    history done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const historyTab = document.getElementById("history-tab");
+const donationTab = document.getElementById("donation-tab");
+historyTab.addEventListener("click", function () {
+  historyTab.classList.add("text-[#111111]", "bg-[#B4F461]");
+
+  donationTab.classList.remove("text-[#111111]", "bg-[#B4F461]");
+
+  donationTab.classList.add("text-[#111111B3]");
+
+  document.getElementById("donation-page").classList.add("hidden");
+  document.getElementById("history-page").classList.remove("hidden");
+});
+
 const donateButtonOne = document.getElementById("donate");
 
 donateButtonOne.addEventListener("click", function () {
@@ -29,6 +42,20 @@ donateButtonOne.addEventListener("click", function () {
   donateMoneyToNoakhali.textContent = newDonatedBalance + " BDT";
 
   document.getElementById("get-donate").value = "";
+
+  const donationLocation = "Noakhali, Bangladesh";
+  const historyItem = document.createElement("div");
+  historyItem.className = "p-4 rounded-lg shadow-md border";
+  historyItem.innerHTML = `
+
+    <h4 class="text-[#111111] font-bold text-xl mb-2">${getDonateNoakhali} Taka is Donated for Flood Relief in ${donationLocation}
+    </h4>
+
+    <h4 class="text-[#111111B3] text-sm font-normal">${new Date().toLocaleString()}</h4>
+  
+  `;
+  const historyWal = document.getElementById("history-page");
+  historyWal.insertBefore(historyItem, historyWal.firstChild);
 });
 
 function updateMainAccounts(newBalance) {
@@ -36,4 +63,4 @@ function updateMainAccounts(newBalance) {
 
   account.textContent = newBalance + " " + "BDT";
 }
-updateMainAccounts(999999);
+updateMainAccounts(10000);
